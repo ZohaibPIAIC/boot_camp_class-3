29da3b747bd104e9434cc977a4325b18e7fb69d8
ZohiabPIAIC <zohaib360arshad@gmail.com>
2020-06-08 01:09:58 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <h1>Hello World From Zohaib Arshad</h1>
+}
+
+
+
+
+  export default App;
